bcbcc5f808dbfae8378fe855aae7ebbf7f5cba0f
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-11 20:38:11 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+class PigLatinizer
+  
+  attr_reader :text
+  
+
+  def piglatinize(text)
+    array = text.split
+    array.map do |word|
+      if word.match(/^[aeiou]/) != nil
+        word << "way"
+      elsif word.match(/^[a-z][aeiou]/) != nil
+          sliced = word.slice!(0)
+          word << "#{sliced}ay"
+      elsif word.match(/^[a-z][a-z][aeiou]/) != nil
+          sliced = word.slice!(0,2)
+          word << "#{sliced}ay"
+      elsif word.match(/^[a-z][a-z][a-z][aeiou]/) !=nil 
+          sliced = word.slice!(0,3)
+          word << "#{sliced}ay"
+        elsif word.match(/^[a-z][a-z][a-z][a-z][aeiou]/) !=nil 
+          sliced = word.slice!(0,4)
+          word << "#{sliced}ay"
+      end
+    end
+    array
+    binding.pry
+  end
+
+  
+end
