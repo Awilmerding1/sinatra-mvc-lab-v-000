e38eaa062e8c479f279ffc66ed529534dc505378
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-11 20:40:56 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,23 +6,23 @@ class PigLatinizer
   def piglatinize(text)
     array = text.split
     array.map do |word|
-      if word.match(/^[aeiou]/) != nil
+      if word.match(/^[aeiouAEIOU]/) != nil
         word << "way"
-      elsif word.match(/^[a-z][aeiou]/) != nil
+      elsif word.match(/^[a-zA-Z][AEIOUaeiou]/) != nil
           sliced = word.slice!(0)
           word << "#{sliced}ay"
-      elsif word.match(/^[a-z][a-z][aeiou]/) != nil
+      elsif word.match(/^[a-zA-Z][a-zA-Z][AEIOUaeiou]/) != nil
           sliced = word.slice!(0,2)
           word << "#{sliced}ay"
-      elsif word.match(/^[a-z][a-z][a-z][aeiou]/) !=nil 
+      elsif word.match(/^[a-zA-Z][A-Za-z][A-Za-z][AEIOUaeiou]/) !=nil 
           sliced = word.slice!(0,3)
           word << "#{sliced}ay"
-        elsif word.match(/^[a-z][a-z][a-z][a-z][aeiou]/) !=nil 
+        elsif word.match(/^[a-zA-Z][A-Za-z][A-Za-z][A-Za-z][AEIOUaeiou]/) !=nil 
           sliced = word.slice!(0,4)
           word << "#{sliced}ay"
       end
     end
-    array.join
+    array.join(" ")
   end
 
   
